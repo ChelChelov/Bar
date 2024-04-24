0a69e6ffdffb8c1145fdb4f374542708d15cf537
Oleg Sazonov <sazonov1994@gmail.com>
2024-04-24 20:59:44 +0300

    add display and filtered functions

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,141 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
-    const cocktails = 
+	const cocktails = 
 	[
-        { name: 'Бульвардьє',
-		  'бурбон': 45,	
-		  'вермут червоний': 30,
-		  'біттер': 30		    
+		{ name: 'Бульвардьє',
+		'бурбон': 45,	
+		'вермут червоний': 30,
+		'біттер': 30		    
+		},
+		{ name: 'Джин Гарден',
+		'водка': 15,	
+		'джин': 50,
+		'бузина сироп': 15,
+		'огурец сироп': 15,
+		'имбирь сироп': 10		    
+		},
+		{ name: 'Ернест',
+		'Темный ром': 40,	
+		'Амаро': 20,
+		'Сироп корица': 20		    
+		  },
+		{ name: 'Занзібар',
+		'Білий ром': 30,	
+		'Темний ром': 30,
+		'Лікер Пізан': 30,
+		'Лікер Кокос': 20,
+		'Сироп Ваніль': 10		    
+		},
+		{ name: 'Зомбі',
+		'Білий ром': 25,	
+		'Темний ром': 25,
+		'Золотий ром': 25,
+		'Лікер тріплсек': 15,
+		'Сироп малина': 10,
+		'Сироп кориця': 10,
+		'Цукровий сироп': 10		    
+		  },
+		{ name: 'Канада',
+		'Бурбон': 20,	
+		'Коньяк': 20,	
+		'Миндаль ликер': 20,
+		'Клен сироп': 20		    
+		},
+		{ name: 'Карпенко Карий',
+		'Горілка': 15,	
+		'Хайзелнат сироп': 7.5,
+		'Кориця сироп': 7.5		    
+		  },
+		{ name: 'Кримський міст',
+		'Горілка': 20,	
+		'Сироп кокос': 5,
+		'Лікер касіс': 5		    
+		},
+		{ name: 'Ла Фемм',
+		'Водка': 50,	
+		'Сироп Бузина': 25,
+		'Экстра-драй вермут': 20		    
+		  },
+		{ name: 'Бейліз Бьюті',
+		'Горілка': 60,	
+		'Сироп маракуя': 15,
+		'Сироп кокос': 15		    
+		},
+		{ name: 'Змієносець',
+		'Горілка': 20,	
+		'Triple sec': 30,
+		'Лікер Диня': 30		    
+		  },
+		{ name: 'Літачок',
+		'Бурбон': 25,	
+		'Чінар': 25,
+		'Апероль': 25		    
+		},
+		{ name: 'Лора Палмер',
+		'Джин': 50,	
+		'Апероль': 30,
+		'Тріпл сек': 15		    
+		},
+		{ name: 'Май Тай',
+		'Білий ром': 30,	
+		'Темний ром': 30,
+		'Золотий ром': 30,
+		'Тріпл сек': 15,		
+		'Лікер Мигдаль': 15,
+		'Карамель сироп': 15		    
 		}
-    ];
+	];
 
-    const cocktailList = document.getElementById('cocktailList');
+	cocktails.sort((a, b) => {
+		const nameA = a.name.toLowerCase();
+		const nameB = b.name.toLowerCase();
+		if (nameA < nameB) return -1;
+		if (nameA > nameB) return 1;
+		return 0;
+	});
+	
+	const cocktailList = document.getElementById('cocktail-list');
 
-    cocktails.forEach(cocktail => {
-        const listItem = document.createElement('li');
-        listItem.textContent = cocktail.name;
-        listItem.classList.add('list__name');
-        cocktailList.appendChild(listItem);
+    // Function to display the list of cocktails
+    function displayCocktails(cocktails) {
+        cocktailList.innerHTML = '';
+        cocktails.forEach(cocktail => {
+            const listItem = document.createElement('li');
+            listItem.textContent = cocktail.name;
+            listItem.classList.add('cocktail-list__name');
+            cocktailList.append(listItem);
+        });
+    }
+
+    // Function to filter cocktails based on user input
+    function filterCocktails(inputText) {
+        const filteredCocktails = cocktails.filter(cocktail => {
+            return cocktail.name.toLowerCase().includes(inputText.toLowerCase());
+        });
+        displayCocktails(filteredCocktails);
+    }
+
+    // Event listener for form submission
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        const inputText = document.getElementById('textInput').value;
+        filterCocktails(inputText);
     });
+
+    // Event listener for input field changes
+    const inputField = document.getElementById('textInput');
+    inputField.addEventListener('input', function() {
+        const inputText = inputField.value;
+        filterCocktails(inputText);
+    });
+
+	displayCocktails(cocktails);
 });
+
+
+//Test zone
 
 const cocktails = 
 [
@@ -121,7 +238,7 @@ function calculateIngredients (total, cocktail) {
 	console.log(newCoctail);
 }
 
-//This function takes an object and return sum of the values of this object if the type of value is number
+//Function to take an object and return sum of the values of this object if the type of value is number
 function sumOfIngredients (cocktail) {
 	let sumOfIngr = 0;
 
